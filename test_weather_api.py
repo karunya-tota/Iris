e19f4b21edd12a32d0a2285d13ed5140cec1b4c6
Karunya Tota <kay.tota@gmail.com>
2017-04-19 12:25:11 -0500

    adding testing for weather api

---
+++ test_weather_api.py
@@ -0,0 +1,43 @@
+import unittest
+from weather_api import *
+
+class TestWeatherAPI(unittest.TestCase):
+    """
+    Open Weather Map API Test Cases Class
+    """
+    def test_missing_prefix(self):
+        '''
+        Tests if the text is processed when the prefix 'Weather at' is missing
+        '''
+        text = 'Weather Orlando'
+        location = find_location(text)
+        self.assertIsNone(location)
+
+        text2 = 'Weather in Orlando'
+        location = find_location(text)
+        self.assertIsNone(location)
+
+    def test_invalid_text(self):
+        '''
+        Tests if an Invalid Request message is returned for invalid location
+        '''
+        text = 'Weather at aslsdkfj'
+        response = get_weather(text)
+        self.assertEqual(response, "Invalid Request. Check syntax and try again!")
+
+    def test_make_request(self):
+        '''
+        Tests if a request is successfully made
+        '''
+        text = 'Weather at Orlando'
+        location = find_location(text)
+        self.assertEqual(location, 'Orlando')
+
+        response = get_weather(text)
+        self.assertIn('Weather:', response)
+        self.assertIn('Description:', response)
+        self.assertIn('Humiditiy:', response)
+        self.assertIn('Wind Speed:', response)
+
+if __name__ == '__main__':
+    unittest.main()
